55922db769c7da19ddf0d7c799739a3dbbef50c0
Piriou SCOTT <pscott@student.42.fr>
2019-05-16 19:04:56 +0200

    fix \0 in non interactive mode

--- srcs/main.c
+++ srcs/main.c
@@ -31,37 +31,36 @@ int		main(int argc, char **argv, char **env)
 	t_st_cmd		*st_cmd;
 	t_vars			vars;
 	int				ret;
-	int				interactive;
 
 	(void)argc;
 	(void)argv;
 	vars.env_vars = 0;
 	vars.shell_vars = 0;
 	vars.hashmap = init_hashmap(INIT_HASH_SIZE);
-	if ((interactive = setup_terminal_settings()) == -1)
+	if (setup_terminal_settings() == -1)
 		return (EXIT_FAILURE);
 	if (!(vars.env_vars = init_env((const char **)env)))
 		return (EXIT_FAILURE);
-	//vars->interactive = interactive;
 	signal_setup();
 	st_cmd = NULL;
 	st_cmd = init_st_cmd((const char **)vars.env_vars);
 	while (42)
 	{
-		if ((ret = input_loop(st_cmd, &vars)) == 0)
+		if ((ret = input_loop(st_cmd, &vars)) == 0 || !*st_cmd->st_txt->txt)
 			break ; // free env, free st_cmd
 		else if (ret > 0 && !is_full_of_whitespaces(st_cmd->st_txt->txt))
 			handle_input(st_cmd, &vars);
 		st_cmd = reset_st_cmd(st_cmd);
 	}
-	ft_printf("exit");
-	print_line();
-	write_to_history(st_cmd, (const char **)vars.env_vars);
+	if (isatty(STDIN_FILENO))
+	{
+		ft_printf("exit");
+		print_line();
+		write_to_history(st_cmd, (const char **)vars.env_vars);
+	}
 	free_all_st_cmds(&st_cmd);
 	free_vars(&vars);
 	delete_hashmap(vars.hashmap);
-	if (reset_terminal_settings() == 0)
-		return (EXIT_FAILURE);
-	else
-		return (EXIT_SUCCESS);
+	reset_terminal_settings();
+	return (EXIT_SUCCESS);
 }

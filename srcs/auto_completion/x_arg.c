e065eaab5ef3833fd290fd9e7cf2d3aa80968144
Axel SCHOENHENZ <aschoenh@e3r12p3.42.fr>
2019-05-26 18:52:48 +0200

    finale merging auto_c / master + handling ~user (sorry for multiple pushes...)

--- srcs/auto_completion/x_arg.c
+++ srcs/auto_completion/x_arg.c
@@ -80,7 +80,7 @@ char				*handle_x_arg(const char *input, const char *to_find_and_next_char)
 	if (input && input[0] && ft_strchr(input, '/'))
 		tmp = ft_strndup(input, ft_strlen(input) - ft_strlen(ft_strrchr(input, '/') + 1));
 	get_path_file_and_to_find(input, &path, &to_find);
-	if (!to_find[0] || is_white_spaces(to_find[0]))
+	if (!to_find[0] || ft_is_white_space(to_find[0]))
 		find_all_except_dots(path, &match);
 	else
 		find_all_match(path, &match, to_find, to_find_and_next_char);

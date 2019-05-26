a43af3b5318ca2742a446fb48aace2d565931b57
pscott <biliklok@yahoo.fr>
2019-05-26 19:09:02 +0200

    remove error opening file message. Closes #15

--- srcs/redirections/redir_great.c
+++ srcs/redirections/redir_great.c
@@ -15,10 +15,7 @@ t_bool		redir_great(t_token *redir, t_token *prev)
 		next = next->next;
 	if ((new_fd = open(next->content, O_WRONLY | O_CREAT | O_TRUNC,
 					S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0)
-	{
-		ft_dprintf(2, "error openening file\n");
 		return (0);
-	}
 	redirect(new_fd, old_fd);
 	redir->type = tk_eat;
 	next->type = tk_eat; // if I dont check is_argv i will segfault on deference

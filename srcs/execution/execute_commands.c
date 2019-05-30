b3a0a981da923aefc36c6c02f78b7acd0fe3c82e
Axel SCHOENHENZ <aschoenh@e1r11p8.42.fr>
2019-05-30 17:13:20 +0200

    norme execution

--- srcs/execution/execute_commands.c
+++ srcs/execution/execute_commands.c
@@ -5,13 +5,14 @@
 #include "cmd_parsing.h"
 #include "errors.h"
 
-static void	access_and_exec(const char *cmd_path, const char **argv, const char **env)
+static void			access_and_exec(const char *cmd_path, const char **argv,
+					const char **env)
 {
-	int	access;
+	int				access;
 
 	if ((access = check_access(cmd_path)) == 0 && reset_terminal_settings())
 	{
-		execve(cmd_path, (char * const*)argv, (char* const*)env);
+		execve(cmd_path, (char*const*)argv, (char*const*)env);
 		print_errors(ERR_EXECUTE, ERR_EXECUTE_STR, cmd_path);
 		clean_exit(1);
 	}
@@ -27,16 +28,16 @@ static void	access_and_exec(const char *cmd_path, const char **argv, const char 
 /*
 **	Execute the argv, first checking if the cmd contains a '/', or if it is
 **	a builtin, if it is in the hashmap, or in the PATH. If it is not found, an
-**	error message is printed. 
+**	error message is printed.
 **	Returns 0 if something was executed ; else, returns the corresponding error
 **	number.
 */
 
 static t_bool		execute_argv(char **argv, t_vars *vars)
 {
-	int		cmd;
-	char	*cmd_path;
-	
+	int				cmd;
+	char			*cmd_path;
+
 	if (!argv)
 		return (1);
 	else if (ft_strchr(argv[0], '/'))
@@ -52,14 +53,16 @@ static t_bool		execute_argv(char **argv, t_vars *vars)
 		;
 	else
 		return (ERR_CMD);
-	access_and_exec(cmd_path, (const char **)argv, (const char**)vars->env_vars);
+	access_and_exec(cmd_path, (const char **)argv,
+			(const char**)vars->env_vars);//good tabulation?
 	ft_strdel(&cmd_path); // useless becasue of exit before
 	return (0);
 }
 
-t_bool		parse_and_exec(t_token *token_head, int in, int out, t_vars *vars)
+t_bool				parse_and_exec(t_token *token_head, int in,
+					int out, t_vars *vars)
 {
-	char		**argv;
+	char			**argv;
 
 	redirect(in, STDIN_FILENO);
 	redirect(out, STDOUT_FILENO);

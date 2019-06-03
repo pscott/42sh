1432d79caf304ae99fc7a26c63c2ae69e763d6e6
Piriou SCOTT <pscott@student.42.fr>
2019-06-03 13:54:16 +0200

    remove useless print_len in input_utils

--- srcs/reader/input_utils.c
+++ srcs/reader/input_utils.c
@@ -51,7 +51,6 @@ void	insert_str(t_st_cmd *st_cmd, const char *buf,
 	st_txt = st_cmd->st_txt;
 	if (!(printable_buf = ft_strdup_print(buf)))
 		ERROR_MEM;
-	print_len = ft_strlen(printable_buf);
 	if (!(end_line = ft_strdup(&st_txt->txt[st_txt->tracker])))
 		ERROR_MEM;
 	ft_strcpy(&st_txt->txt[st_txt->tracker + print_len], end_line);

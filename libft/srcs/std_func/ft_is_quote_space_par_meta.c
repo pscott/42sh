ee180b967b2af00adfde072734e485cd2e2aae96
Piriou SCOTT <pscott@student.42.fr>
2019-06-03 14:21:47 +0200

    fix norme in libft

--- libft/srcs/std_func/ft_is_quote_space_par_meta.c
+++ libft/srcs/std_func/ft_is_quote_space_par_meta.c
@@ -6,7 +6,7 @@
 /*   By: aschoenh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 19:20:30 by aschoenh          #+#    #+#             */
-/*   Updated: 2019/05/24 19:57:36 by penzo            ###   ########.fr       */
+/*   Updated: 2019/06/03 14:21:37 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ unsigned int	ft_is_quote(char c)
 
 unsigned int	ft_is_white_space(char c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\r')//test \r: useless
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r')
 		return (1);
 	return (0);
 }
 
-unsigned int	ft_is_parenth(char c)//useless ? or lexer error when encountered ?
+unsigned int	ft_is_parenth(char c)
 {
 	if (c == '[' || c == ']' || c == '(' || c == ')')
 		return (1);

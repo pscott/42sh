7bc5bbef11530b871d3b555d8250f2a610a8b05b
Piriou SCOTT <pscott@student.42.fr>
2019-06-04 16:49:27 +0200

    norminette libterm

---
+++ libterm/srcs/check_caps.c
@@ -0,0 +1,62 @@
+#include "libterm.h"
+#include "cursor.h"
+
+static void		fill_res(char ***res)
+{
+	if (!((*res)[0] = ft_strdup(BEGIN_LINE))
+		|| !((*res)[1] = ft_strdup(LEFT_CORNER))
+		|| !((*res)[2] = ft_strdup(MOVE_CURSOR))
+		|| !((*res)[3] = ft_strdup(CLEAR))
+		|| !((*res)[4] = ft_strdup(SAVE_CURSOR))
+		|| !((*res)[5] = ft_strdup(CLEAR_BELOW))
+		|| !((*res)[6] = ft_strdup(RESTORE_CURSOR))
+		|| !((*res)[7] = ft_strdup(INVISIBLE))
+		|| !((*res)[8] = ft_strdup(VISIBLE))
+		|| !((*res)[9] = ft_strdup(PRINT_LINE))
+		|| !((*res)[10] = ft_strdup(ERASE_ENDLINE))
+		|| !((*res)[11] = ft_strdup(HIGHLIGHT))
+		|| !((*res)[12] = ft_strdup(NO_HIGHLIGHT))
+		|| !((*res)[13] = ft_strdup(UNDERLINE))
+		|| !((*res)[14] = ft_strdup(NO_UNDERLINE))
+		|| !((*res)[15] = ft_strdup(SCROLL_DOWN))
+		|| !((*res)[16] = ft_strdup(MOVE_UP)))
+	{
+		ft_free_ntab(*res);
+		*res = NULL;
+	}
+}
+
+static char		**create_cap_array(void)
+{
+	char	**res;
+
+	if (!(res = (char**)malloc(sizeof(*res) * (NUM_CAPS + 1))))
+		return (NULL);
+	ft_bzero(res, sizeof(*res) * (NUM_CAPS + 1));
+	fill_res(&res);
+	return (res);
+}
+
+int				check_caps(void)
+{
+	char	**caps;
+	int		i;
+
+	if (!(caps = create_cap_array()))
+	{
+		ft_putstr_fd("error: failed to allocate memory\n", 2);
+		exit(1);
+	}
+	i = 0;
+	while (i < NUM_CAPS)
+	{
+		if (tgetstr(caps[i], NULL) == NULL)
+		{
+			ft_free_ntab(caps);
+			return (0);
+		}
+		i++;
+	}
+	ft_free_ntab(caps);
+	return (1);
+}

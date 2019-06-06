b9470f448f109ecf633d354e607f537010b9bf84
El famoso MPorzier <mporzier@e3r12p4.42.fr>
2019-06-06 18:10:05 +0200

    fix introduction for new shortcuts

--- srcs/introduction.c
+++ srcs/introduction.c
@@ -9,13 +9,13 @@ void		print_introduction(void)
 	ft_printf("           |_____|_|___/_| |_|\n\n");
 	ft_printf("`Left arrow` to move the cursor to te left\n");
 	ft_printf("'Right arrow` to move the cursor to the right\n");
-	ft_printf("`Home` to move the cursor to the beginning of the line\n");
-	ft_printf("`End` to move the cursor to the end of the line\n");
 	ft_printf("`Up arrow` to display the previous history entry\n");
 	ft_printf("`Down arrow` to display the next history entry\n");
-	ft_printf("`Ctrl + b` to move the cursor to the previous word\n");
-	ft_printf("`Ctrl + f` to move the cursor to the next word\n");
+	ft_printf("`Home` to move the cursor to the beginning of the line\n");
+	ft_printf("`End` to move the cursor to the end of the line\n");
 	ft_printf("`Ctrl + r` to search a command in the history\n");
 	ft_printf("`Option + up` to move to the upper line\n");
 	ft_printf("`Option + down` to move to the downer line\n");
+	ft_printf("`Option + left` to move the cursor to the previous word\n");
+	ft_printf("`Option + right` to move the cursor to the next word\n");
 }

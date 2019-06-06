ff3fc964f3f4eff8319600a3aadef1f2fb19427d
Piriou SCOTT <pscott@student.42.fr>
2019-06-06 21:11:20 +0200

    remove going to end of line on ctrl + c

--- srcs/signals/signals_handlers.c
+++ srcs/signals/signals_handlers.c
@@ -34,12 +34,7 @@ void			sigint_handler(int signo)
 	vars = get_vars(NULL);
 	vars->cmd_value = 1;
 	if (isatty(STDIN_FILENO))
-	{
 		write(STDERR, "^C", 2);
-		st_cmd->hist_lst = get_end_lst(st_cmd->hist_lst);
-		get_pos(st_cmd, st_cmd->st_txt->data_size);
-		reposition_cursor(st_cmd);
-	}
 	execute_str(PRINT_LINE);
 	execute_str(BEGIN_LINE);
 	execute_str(CLEAR_BELOW);

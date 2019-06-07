ab001791b85d77e3d195d730f875cf17391eae1c
Piriou SCOTT <pscott@student.42.fr>
2019-06-07 09:14:14 +0200

    norme libft

--- libft/srcs/std_func/ft_strrnstr.c
+++ libft/srcs/std_func/ft_strrnstr.c
@@ -6,7 +6,7 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 14:53:19 by pscott            #+#    #+#             */
-/*   Updated: 2019/06/06 18:50:55 by aschoenh         ###   ########.fr       */
+/*   Updated: 2019/06/07 09:13:10 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,7 @@ char	*ft_strrnstr(const char *haystack, const char *needle, size_t len)
 
 	if (*needle == 0)
 		return (NULL);
-	i = len;
-	/*
-	ft_printf("\n\n1:|%s|, 2:|%s|, len:|%d|\n\n", haystack, needle, len);
-	sleep(3);
-	*/
-	if (i > ft_strlen(haystack) - 1)
-		i = ft_strlen(haystack) - 1;
+	i = len > ft_strlen(haystack) - 1 ? ft_strlen(haystack) - 1 : len;
 	while (haystack[i])
 	{
 		j = 0;

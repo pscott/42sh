8caa21fc294f69df28d55660a1646e6c9c6ec1e3
Piriou SCOTT <pscott@student.42.fr>
2019-06-07 09:15:31 +0200

    norminette libterm

--- libterm/includes/libterm.h
+++ libterm/includes/libterm.h
@@ -6,7 +6,7 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 14:54:40 by pscott            #+#    #+#             */
-/*   Updated: 2019/06/06 20:58:24 by pscott           ###   ########.fr       */
+/*   Updated: 2019/06/07 09:14:54 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,8 +84,6 @@
 # define ALT_RIGHT_LEN 4
 # define ALT_LEFT "\x1b\x1b\x5b\x44"
 # define ALT_LEFT_LEN 4
-
-
 
 /*
 ** Previous terminal settings are saved in a global, and restored on exit.

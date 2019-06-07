3f2588ad239988f093fd2067a4d51009eef70218
Piriou ENZO <penzo@student.42.fr>
2019-06-07 15:51:45 +0200

    fix max long long in exp_arith

---
+++ srcs/expands/special_lltoa.c
@@ -0,0 +1,59 @@
+#include "cmd_parsing.h"
+#include "exp_arith.h"
+
+static size_t	get_ll_len(long long nb)
+{
+	size_t	len;
+
+	len = (nb < 0) ? 1 : 0;
+	if (nb == 0)
+		return (1);
+	while (nb)
+	{
+		len++;
+		nb = nb / 10;
+	}
+	return (len);
+}
+
+static char		*check_min(long long nb)
+{
+	char *str;
+
+	if ((unsigned long long)nb == -9223372036854775808U)
+	{
+		if (!(str = ft_strdup("-9223372036854775808")))
+			clean_exit(1, 1);
+		return (str);
+	}
+	return (NULL);
+}
+
+char			*lltoa_and_free(long long nb, char **to_free)
+{
+	char		*str;
+	int			len;
+
+	if ((str = check_min(nb)))
+		return (str);
+	ft_strdel(to_free);
+	len = get_ll_len(nb);
+	if (!(str = ft_strnew(len)))
+		clean_exit(1, 1);
+	if (nb == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb)
+	{
+		str[len-- - 1] = nb % 10 + '0';
+		nb = nb / 10;
+	}
+	return (str);
+}

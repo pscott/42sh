091421105faa81a44b4b91c5b4a51e479e227145
Piriou SCOTT <pscott@student.42.fr>
2019-06-08 18:37:56 +0200

    add bell sound when reaching end or start of line when jumping words

--- srcs/reader/check_words.c
+++ srcs/reader/check_words.c
@@ -4,12 +4,18 @@ int	check_for_words(t_st_cmd *st_cmd, const char *buf)
 {
 	if (ft_strncmp(buf, ALT_LEFT, ALT_LEFT_LEN + 1) == 0)
 	{
-		jump_words(st_cmd, -1);
+		if (st_cmd->st_txt->tracker == 0)
+			ft_putstr(BELL);
+		else
+			jump_words(st_cmd, -1);
 		return (1);
 	}
 	else if (ft_strncmp(buf, ALT_RIGHT, ALT_RIGHT_LEN + 1) == 0)
 	{
-		jump_words(st_cmd, 1);
+		if (st_cmd->st_txt->tracker == st_cmd->st_txt->data_size)
+			ft_putstr(BELL);
+		else
+			jump_words(st_cmd, 1);
 		return (1);
 	}
 	else

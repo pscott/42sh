8784f1236bcb160a093e0a132585d08f98f28079
Piriou ENZO <penzo@student.42.fr>
2019-06-08 19:26:09 +0200

    fix double error print on 'cat < NO_EXIST'

--- srcs/redirections/redir_less.c
+++ srcs/redirections/redir_less.c
@@ -3,12 +3,26 @@
 #include "libterm.h"
 #include <fcntl.h>
 
+static int	print_fd_error(int fd, int mode)
+{
+	if (mode != -1)
+		ft_dprintf(2, "42sh: %d: bad file descriptor\n", fd);
+	return (1);
+}
+
+static int	print_file_error(char *filename, int mode)
+{
+	if (mode != -1)
+		ft_dprintf(2, "42sh: %s: No such file or directory\n", filename);
+	return (1);
+}
+
 /*
 **	Returns 0 on success
 **	Else returns error number
 */
 
-int		redir_less(t_token *redir, t_token *prev, int mode)
+int			redir_less(t_token *redir, t_token *prev, int mode)
 {
 	int		old_fd;
 	t_token	*next;
@@ -17,20 +31,14 @@ int		redir_less(t_token *redir, t_token *prev, int mode)
 	if ((old_fd = check_fd_prev(prev)) < 0)
 		old_fd = STDIN_FILENO;
 	if (old_fd > 9)
-	{
-		ft_dprintf(2, "42sh: %d: bad file descriptor\n", old_fd);
-		return (1);
-	}
+		return (print_fd_error(old_fd, mode));
 	next = redir->next;
 	while (next->type == tk_eat)
 		next = next->next;
 	redir->type = tk_eat;
 	next->type = tk_eat;
 	if ((new_fd = open(next->content, O_RDONLY)) < 0)
-	{
-		ft_dprintf(2, "42sh: %s: No such file or directory\n", next->content);
-		return (1);
-	}
+		return (print_file_error(next->content, mode));
 	redirect(new_fd, old_fd, mode);
 	return (0);
 }

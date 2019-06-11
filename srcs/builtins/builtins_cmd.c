5c187c2c716597b10b8769f242dcc02e6756764a
Axel SCHOENHENZ <axel.schoenhenz@gmail.com>
2019-06-11 13:23:05 +0200

    parsing flags good

--- srcs/builtins/builtins_cmd.c
+++ srcs/builtins/builtins_cmd.c
@@ -13,8 +13,6 @@ int			exec_builtins(char **argv, t_vars *vars, t_cmd_id cmd_id)
 {
 	int				ret;
 
-	ft_dprintf(2, "ROEORE");
-	sleep(20);
 	if (!argv)
 		ret = 1;
 	else if (cmd_id == cmd_exit)
@@ -48,8 +46,6 @@ int			exec_builtins(char **argv, t_vars *vars, t_cmd_id cmd_id)
 
 int			check_builtins(char **argv)
 {
-	ft_dprintf(2, "ROEORE");
-	sleep(2);
 	if (ft_strcmp(argv[0], "exit") == 0)
 		return (cmd_exit);
 	if (ft_strcmp(argv[0], "env") == 0)

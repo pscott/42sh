26af065fc9a0c256aa4489ad0095ed83fa34e248
Piriou SCOTT <pscott@student.42.fr>
2019-06-14 09:50:48 +0200

    add check for words

--- srcs/reader/selector.c
+++ srcs/reader/selector.c
@@ -3,7 +3,7 @@
 
 void	selector(t_st_cmd *st_cmd, char *buf, t_vars *vars)
 {
-	if (check_for_arrows(st_cmd, buf, vars))
+	if (check_for_arrows(st_cmd, buf, vars) || check_for_words(st_cmd, buf))
 	{
 		vars->select_end = st_cmd->st_txt->tracker;
 		reposition_cursor(st_cmd, 0);

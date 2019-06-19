4e9027818b0477073db3f381b77686caaa771a68
Axel SCHOENHENZ <axel.schoenhenz@gmail.com>
2019-06-19 13:52:20 +0200

    remove introduction ; check winsize > 0 before / or %

--- srcs/line_editing/st_cmd_windows_struct_utils.c
+++ srcs/line_editing/st_cmd_windows_struct_utils.c
@@ -14,7 +14,7 @@ void			init_relative_pos(t_pos *cursor_pos, struct winsize *window,
 		cursor_pos->col = prompt_size;
 		cursor_pos->row = prompt_size;
 	}
-	else
+	else if (widows->ws_col > 0)
 	{
 		cursor_pos->col = prompt_size % window->ws_col;
 		cursor_pos->row = prompt_size / window->ws_col;

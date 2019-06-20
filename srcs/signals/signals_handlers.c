686dacc90c4d0b8a4e8959b9e4f22370dc593c09
Piriou SCOTT <pscott@student.42.fr>
2019-06-20 18:00:18 +0200

    remove writing \x03 when ctrl c

--- srcs/signals/signals_handlers.c
+++ srcs/signals/signals_handlers.c
@@ -41,8 +41,8 @@ void			sigint_handler(int signo)
 	if (!(st_cmd = get_st_cmd(NULL)))
 		return ;
 	st_cmd = get_last_st_cmd(st_cmd);
-	if (st_cmd->st_txt->txt)
-		*st_cmd->st_txt->txt = '\x03';
+	/*if (st_cmd->st_txt->txt)
+		*st_cmd->st_txt->txt = '\x03';*/
 	if ((vars = get_vars(NULL)))
 		vars->cmd_value = 1;
 	if (isatty(TERM_FD))

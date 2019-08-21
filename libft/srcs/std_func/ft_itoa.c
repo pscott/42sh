68750dae5653127755f2d908473d5265294ba66a
pscott <pscott@student.42.fr>
2019-08-21 17:35:21 +0200

    ft_itoa returns INT_MIN on INT_MIN input, instead of crashing

--- libft/srcs/std_func/ft_itoa.c
+++ libft/srcs/std_func/ft_itoa.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-static int	get_my_len(int n)
+static int	get_num_len(int n)
 {
 	int len;
 
@@ -29,31 +29,16 @@ static void	fill_itoa(int n, char *src, int len)
 	src[0] = n + '0';
 }
 
-static char	*min_int(char *res)
-{
-	char	*num;
-	int		i;
-
-	num = "-2147483648";
-	i = 0;
-	while (i < 11)
-	{
-		res[i] = num[i];
-		i++;
-	}
-	return (res);
-}
-
 char		*ft_itoa(int n)
 {
 	char	*res;
 	int		len;
 
-	len = get_my_len(n);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = get_num_len(n);
 	if (!(res = ft_strnew(len)))
 		return (NULL);
-	if (n == -2147483648)
-		return (min_int(res));
 	if (n < 0)
 	{
 		res[0] = '-';
